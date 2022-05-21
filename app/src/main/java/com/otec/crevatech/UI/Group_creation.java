161dcf3b5b5aa4c6e1152359252175c7e1c305f1
Tobe <gregokko18@gmail.com>
2022-05-21 08:54:19 +0100

    added progress bar spinner

--- app/src/main/java/com/otec/crevatech/UI/Group_creation.java
+++ app/src/main/java/com/otec/crevatech/UI/Group_creation.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -33,6 +34,7 @@ public class Group_creation extends Fragment {
 
     private Button group_create;
     private EditText groupName,Amount,Liquidator_size,miner_stake;
+    private ProgressBar  spinners;
 
     private FirebaseFirestore firestore;
     private String TAG = "Group_creation";
@@ -45,6 +47,7 @@ public class Group_creation extends Fragment {
         Amount = view.findViewById(R.id.Amount);
         Liquidator_size = view.findViewById(R.id.Liquidator_size);
         miner_stake = view.findViewById(R.id.miner_stake);
+        spinners = view.findViewById(R.id.spinners);
         firestore = FirebaseFirestore.getInstance();
 
 
@@ -57,23 +60,30 @@ public class Group_creation extends Fragment {
         return view;
     }
 
+
+
+
     private boolean Val(EditText groupName, EditText threshold, EditText liquidator_stake, EditText miner_stake) {
         return groupName.getText().toString().trim().length() > 0 && threshold.getText().toString().trim().length() > 0 && liquidator_stake.getText().toString().trim().length() > 0 && miner_stake.getText().toString().trim().length() > 0;
     }
 
 
     private void Check() {
+        spinners.setVisibility(View.VISIBLE);
         Request_class request_class = Base_config.getRetrofit().create(Request_class.class);
         Call<Map<String,Object>> isFunded = request_class.SendGroupRequest(new utilJava().GET_GROUP(new utilJava().GET_CACHED_MAP(getContext(),getString(R.string.SIGNED_IN_USER)),groupName,Amount,Liquidator_size,miner_stake));
         isFunded.enqueue(new Callback<Map<String,Object>>() {
             @Override
             public void onResponse(Call<Map<String,Object>> call, Response<Map<String,Object>> response) {
                  new utilKotlin().message2(response.body().get("message").toString(),getContext());
+                spinners.setVisibility(View.INVISIBLE);
+                groupName.setText(""); Amount.setText(""); Liquidator_size.setText(""); miner_stake.setText("");
             }
             @Override
             public void onFailure(Call<Map<String,Object>> call, Throwable t) {
                 new utilKotlin().message2(t.getMessage(), getContext());
                 Log.d(TAG, "onFailure:  " + t.getMessage());
+                spinners.setVisibility(View.INVISIBLE);
             }
         });
 
